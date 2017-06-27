cad97ca0edde699633e97384bdfe6af777515ca5
Andrew Glines and Halle Tempest <andrew+halle@>
2017-06-27 14:58:53 -0700

    add jshint task to gulpfile.js

--- js/clock-interface.js
+++ js/clock-interface.js
@@ -3,7 +3,7 @@ var Time = require('./../js/clock.js').timeModule;
 $(document).ready(function(){
   var currentTimeObject = new Time();
 
-  var now = moment();
+  var now = moment().format(hh:mm:ss);
   // console.log(now);
   currentTimeObject.getTime(now);
 })
